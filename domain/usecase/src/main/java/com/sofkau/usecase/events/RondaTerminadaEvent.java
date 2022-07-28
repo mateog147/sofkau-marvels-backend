bc1651c8867cf7af4266a94b98fef2b3ee465c30
mateog147 <mateog147@hotmail.com>
2022-07-28 17:27:36 -0500

    web socket de ganador

---
+++ domain/usecase/src/main/java/com/sofkau/usecase/events/RondaTerminadaEvent.java
@@ -0,0 +1,12 @@
+package com.sofkau.usecase.events;
+
+import com.sofkau.model.ronda.Ronda;
+import org.springframework.context.ApplicationEvent;
+
+
+public class RondaTerminadaEvent extends ApplicationEvent {
+    public RondaTerminadaEvent(Ronda ronda) {
+        super(ronda);
+        System.out.println("Ronda Terminada Event");
+    }
+}
--- domain/usecase/src/main/java/com/sofkau/usecase/ronda/ganadorronda/GanadorRondaUseCase.java
+++ domain/usecase/src/main/java/com/sofkau/usecase/ronda/ganadorronda/GanadorRondaUseCase.java
@@ -2,11 +2,15 @@ package com.sofkau.usecase.ronda.ganadorronda;
 
 import com.sofkau.model.ronda.Ronda;
 import com.sofkau.model.ronda.gateways.RondaRepository;
+import com.sofkau.usecase.events.ApuestaAgregaEvent;
+import com.sofkau.usecase.events.RondaTerminadaEvent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 public class GanadorRondaUseCase {
+    private final ApplicationEventPublisher publisher;
     private final RondaRepository repository;
 
     public Mono<Ronda> terminarRonda (Ronda ronda, String nombreGanador) {
@@ -15,6 +19,7 @@ public class GanadorRondaUseCase {
             ronda.next();
             ronda.setUltimoGanador(nombreGanador);
         }
-        return repository.save(ronda);
+        return repository.save(ronda)
+                .doOnSuccess(ronda1 -> this.publisher.publishEvent(new RondaTerminadaEvent(ronda1)));
     }
 }
